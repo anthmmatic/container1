f7d4bfb2106c2665045f45e74ef8d993cc74b389
Anthony <86444119+anthms@users.noreply.github.com>
2025-11-10 14:04:01 -0600

    Fix directory check and copy command in copydata.sh

--- copydata.sh
+++ copydata.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 set -e
-if [ -d /data ]; then
+if [ -d /data/upstream ]; then
   # copy once if target is empty
   if [ -z "$(ls -A /opt/upstream 2>/dev/null)" ]; then
-    cp -a /data/. /opt/upstream/
+    cp -a /data/upstream. /opt/upstream/
   fi
 fi
 exec "$@"
