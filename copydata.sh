b7091fedcee4f14515eb35fdb864da94af295c06
Anthony <86444119+anthms@users.noreply.github.com>
2025-11-10 14:04:44 -0600

    Fix copy command syntax in copydata.sh

--- copydata.sh
+++ copydata.sh
@@ -4,7 +4,7 @@ set -e
 if [ -d /data/upstream ]; then
   # copy once if target is empty
   if [ -z "$(ls -A /opt/upstream 2>/dev/null)" ]; then
-    cp -a /data/upstream. /opt/upstream/
+    cp -a /data/upstream/. /opt/upstream/
   fi
 fi
 exec "$@"
